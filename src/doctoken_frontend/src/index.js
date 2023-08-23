b6a49de0dd973a765193097a77e31e62064e497a
Ilia <justloreland@gmail.com>
2023-08-23 12:34:06 +0400

    add favicon

--- src/doctoken_frontend/src/index.js
+++ src/doctoken_frontend/src/index.js
@@ -7,13 +7,13 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   const name = document.getElementById("name").value.toString();
   const author = document.getElementById("author").value.toString();
   const description = document.getElementById("description").value.toString();
-  const hashsum = document.getElementById("hashsum").value.toString();
+  const checksum = document.getElementById("checksum").value.toString();
   const link = document.getElementById("link").value.toString();
 
   button.setAttribute("disabled", true);
 
   // Interact with the Dip721NFT actor, calling the mintNFT method
-  const receipt = await doctoken_backend.mintNFT(name, author, description, hashsum, link);
+  const receipt = await doctoken_backend.mintNFT(name, author, description, checksum, link);
 
   button.removeAttribute("disabled");
 
