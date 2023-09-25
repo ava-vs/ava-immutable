a64fbde38fa9dee3d30a4d0a3b3b75cf01347983
aVa <info@ava.capetown>
2023-09-25 09:57:59 +0300

    backend for IC deployment

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,6 @@ dfx stop
 
 dfx start --clean --background
 
-dfx canister create doctoken_backend
-dfx canister create doctoken_frontend
-
-dfx build
-
-dfx canister install doctoken_frontend
-
 dfx deploy --argument "(principal\"$(dfx identity get-principal)\")" doctoken_backend
 
 echo "Creating NFT"
